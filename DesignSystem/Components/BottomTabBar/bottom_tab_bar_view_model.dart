61db7bfe4ffe43159fad2baec620ed9caa5c34f4
ANDRÉ DA SILVA ALVES <andre.salves@a.catolica-to.edu.br>
2024-09-25 18:58:10 -0300

    Implementação do componente TabBar

---
+++ DesignSystem/Components/BottomTabBar/bottom_tab_bar_view_model.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+class BottomTabBarViewModel {
+  final Function(int)? onIndexChanged;
+  final List<BottomNavigationBarItem> bottomTabs;
+
+  BottomTabBarViewModel({
+    required this.bottomTabs, 
+    this.onIndexChanged
+  });
+}
