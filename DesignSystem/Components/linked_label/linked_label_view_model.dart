2a0eb0c0f719f584040adab4023edf197acd5d7a
Mateus <mateuscitriniti@gmail.com>
2024-10-02 09:40:55 -0300

    Adiçao de componente LinkedLabel

---
+++ DesignSystem/Components/linked_label/linked_label_view_model.dart
@@ -0,0 +1,7 @@
+// Opcional: Se você precisar de lógica adicional relacionada ao link, como o URL,
+// você pode criar um view model.
+class LinkedLabelViewModel {
+  final String url;
+
+  LinkedLabelViewModel({required this.url});
+}
